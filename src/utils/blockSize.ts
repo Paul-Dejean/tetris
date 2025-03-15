f7267a9e2c6caa037f47b847c0130a52351a851e
Ebooth <pauldejeandev@gmail.com>
2025-03-15 15:17:06 +0100

    More responsive

--- src/utils/blockSize.ts
+++ src/utils/blockSize.ts
@@ -1,4 +1,7 @@
 export function getBlockSize() {
+  if (window.matchMedia("(max-height: 45rem)").matches) {
+    return 16;
+  }
   if (window.matchMedia("(max-width: 48rem)").matches) {
     return 26;
   }
