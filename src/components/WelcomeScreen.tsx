157dc127934ac505983f2964931128cec1c748e1
Ebooth <pauldejeandev@gmail.com>
2025-03-15 14:57:01 +0100

    Fix wallpaper not displayed

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -2,7 +2,7 @@ import { Button } from "./Button";
 
 export function WelcomeScreen({ onStartClick }: { onStartClick: () => void }) {
   return (
-    <div className="flex flex-col items-center justify-center  h-full gap-y-16 bg-[url('welcome-wallpaper.webp')] bg-cover bg-center">
+    <div className="flex flex-col items-center justify-center  h-full gap-y-16 bg-[url('/welcome-wallpaper.webp')] bg-cover bg-center">
       <div className="container flex flex-col items-center gap-y-8">
         <div className="bg-background flex flex-col gap-y-8 items-center p-8 rounded-xl">
           <h1 className="text-4xl font-bold text-primary">Welcome to tetris</h1>
